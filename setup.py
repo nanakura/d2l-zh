d51c81396b1e794b6665f8f37ab6c4ca3b411ac4
WSRer <69560752+nanakura@users.noreply.github.com>
2025-01-15 20:31:09 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from setuptools import setup, find_packages
 import d2l
 
 requirements = [
-    'jupyter==1.0.0',
-    'numpy==1.21.5',
-    'matplotlib==3.5.1',
-    'requests==2.25.1',
-    'pandas==1.2.4'
+    'jupyter==1.1.1',
+    'numpy==2.2.1',
+    'matplotlib==3.10.0',
+    'requests==2.32.3',
+    'pandas==2.2.3'
 ]
 
 setup(
